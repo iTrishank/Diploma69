528e1ce11e3a9a7e4f66ddab51aab2dc6b976714
tsingkh <tsingkh@samokat.ru>
2023-05-17 15:20:08 +0300

    T-03 Autharization Modal, Login and SignUp Page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,12 @@ import React from "react";
 import whiteLogo from "../images/tinder_logo_white.png";
 import colorLogo from "../images/color-logo-tinder.png";
 
-const Nav = ({ minimal, authToken }) => {
+const Nav = ({ minimal, authToken, setShowModal, showModal, setIsSignUp }) => {
+  //* To show the modal from "Login button"
+  const handleClick = () => {
+    setShowModal(true);
+    setIsSignUp(false);
+  };
   return (
     <nav>
       <div className="logo-container">
@@ -12,7 +17,15 @@ const Nav = ({ minimal, authToken }) => {
           alt="noImg"
         />
       </div>
-      {!authToken && <button className="nav-button">Log In</button>}
+      {!authToken && (
+        <button
+          className="nav-button"
+          onClick={handleClick}
+          disabled={showModal}
+        >
+          Log In
+        </button>
+      )}
     </nav>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,42 @@
 import React from "react";
 import Nav from "../components/Nav";
+import { useState } from "react";
+import AuthModal from "../components/AuthModal";
 
 const Home = () => {
-  //Authorization taken is added here
+  //* Authorization taken is added here
   const authToken = false;
 
-  //Primaru button functionality
+  const [showModal, setShowModal] = useState(false);
+
+  //* Where the state of signup is stored
+  const [isSignUp, setIsSignUp] = useState(true);
+
+  //* Primary button functionality
   const handleClick = () => {
     console.log("clicked!");
+    setShowModal(true);
+    setIsSignUp(true);
   };
 
   return (
     <div className="overlay">
-      <Nav minimal={false} authToken={authToken} />
+      <Nav
+        minimal={false}
+        authToken={authToken}
+        setShowModal={setShowModal}
+        showModal={showModal}
+        setIsSignUp={setIsSignUp}
+      />
       <div className="home">
-        <h1>Swipe Right</h1>
+        <h1 className="primary-title">Swipe Right</h1>
         <button className="primary-button" onClick={handleClick}>
           {authToken ? "Signout" : "Create Account"}
         </button>
+
+        {showModal && (
+          <AuthModal setShowModal={setShowModal} isSignUp={isSignUp} />
+        )}
       </div>
     </div>
   );
