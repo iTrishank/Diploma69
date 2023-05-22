348221a48745b0b413c4b94cbb407612c4043031
tsingkh <tsingkh@samokat.ru>
2023-05-23 01:33:28 +0300

    T-11 Dashboard complete 50%

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,23 @@ import OnBoarding from "./pages/OnBoarding";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
+  //* Get cookie value by name
+  const getCookie = (name) => {
+    const cookies = document.cookie.split("; ").reduce((acc, cookie) => {
+      const [cookieName, cookieValue] = cookie.split("=");
+      acc[cookieName] = cookieValue;
+      return acc;
+    }, {});
+
+    return cookies[name] || "";
+  };
+  const authToken = getCookie("AuthToken");
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path="/OnBoarding" element={<OnBoarding />} />
+        {authToken && <Route path="/Dashboard" element={<Dashboard />} />}
+        {authToken && <Route path="/OnBoarding" element={<OnBoarding />} />}
       </Routes>
     </BrowserRouter>
   );
