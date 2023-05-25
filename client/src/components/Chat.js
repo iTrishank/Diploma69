d502c51187382be6c151d3905613a96dfbd868ba
tsingkh <tsingkh@samokat.ru>
2023-05-25 04:22:21 +0300

    T-18 Make up in process

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -6,13 +6,21 @@ const Chat = ({ descendingOrderMessages }) => {
       <div className="chat-display">
         {descendingOrderMessages.map((message, _index) => (
           <div key={_index}>
-            <div className="chat-message-header">
+            <div
+              className="chat-message-header"
+              style={{ borderTop: "1px solid grey" }}
+            >
               <div className="img-container">
-                <img src="message.img" alt={message.first_name + " profile"} />
+                <img src={message.img} alt={message.first_name + " profile"} />
               </div>
-              <p>{message.name}</p>
+              <p style={{ fontSize: "25px" }}>{message.name}</p>
             </div>
-            <p>{message.message}</p>
+            <p
+              className="mess"
+              style={{ marginLeft: "15px", color: "rgb(36, 36, 36)" }}
+            >
+              {message.message}
+            </p>
           </div>
         ))}
       </div>
